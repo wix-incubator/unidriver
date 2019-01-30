7fd684dbfbb5dda1e47321e92f8e4cd2adfc1b82
Adi Tanami <adit@wix.com>
2019-01-30 15:47:53 +0200

    lint and naming fixes

--- src/lib/puppeteer/index.ts
+++ src/lib/puppeteer/index.ts
@@ -1,174 +1,177 @@
 import { Locator, UniDriverList, UniDriver, MapFn } from '..';
 import { ElementHandle, Page } from 'puppeteer';
-import {waitFor} from '../../utils';
-// import {element} from 'prop-types';
-// import { waitFor } from '../../utils';
+import { waitFor } from '../../utils';
 
-type ElementContainer = {element: ElementHandle | null, page: Page, selector: string}
-type ElementsContainer = {elements: ElementHandle[], page: Page, selector: string}
-
-type PageAndSelector = {page: Page, selector: string};
+type BaseElementContainer = { page: Page; selector: string };
+type ElementContainer = BaseElementContainer & { element: ElementHandle | null };
+type ElementsContainer = BaseElementContainer & { elements: ElementHandle[] };
 
 type ElementGetter = () => Promise<ElementContainer>;
 type ElementsGetter = () => Promise<ElementsContainer>;
 
-export const pupUniDriverList = (elems: ElementsGetter): UniDriverList<ElementContainer> => {
+export const pupUniDriverList = (
+  elems: ElementsGetter
+): UniDriverList<ElementContainer> => {
+  const map = async <T>(fn: MapFn<T>) => {
+    const {elements, ...rest} = await elems();
+    const promises = elements.map((element, i) => {
+      const bd = pupUniDriver(() => Promise.resolve({element, ...rest}));
+      return fn(bd, i);
+    });
+    return Promise.all(promises);
+  };
 
-	const map = async <T>(fn: MapFn<T>) => {
-		const {elements, ...rest} = await elems();
-		const promises = elements.map((element, i) => {
-			const bd = pupUniDriver(() => 
-				Promise.resolve({element, ...rest})
-			);
-			return fn(bd, i);
-		});
-		return Promise.all(promises);
-	};
-
-	return {
-		get: (idx: number) => {
-			const elem = async () => {
-				const {elements, ...rest} =  await elems();
-				return {
-					element: elements[idx],
-					...rest,
-				};
-			};
-			return pupUniDriver(elem);
-		},
-		text: async () => {
-			return map((d) => d.text());
-		},
-		count: async () => {
-			const {elements} = await elems();
-			return elements.length;
-		},
-		map,
-		filter: (fn) => {
-			return pupUniDriverList(async () => {
-				const {elements, ...rest} = await elems();
-				const results = await map(fn);
-				const filteredElements = elements.filter((_, i) => {
-					return results[i];
-				});
-				return {
-					elements: filteredElements,
-					...rest
-				}
-
-			});
-		}
-	};
+  return {
+    get: (idx: number) => {
+      const elem = async () => {
+        const {elements, ...rest} = await elems();
+        return {
+          element: elements[idx],
+          ...rest
+        };
+      };
+      return pupUniDriver(elem);
+    },
+    text: async () => {
+      return map((d) => d.text());
+    },
+    count: async () => {
+      const {elements} = await elems();
+      return elements.length;
+    },
+    map,
+    filter: (fn) => {
+      return pupUniDriverList(async () => {
+        const {elements, ...rest} = await elems();
+        const results = await map(fn);
+        const filteredElements = elements.filter((_, i) => {
+          return results[i];
+        });
+        return {
+          elements: filteredElements,
+          ...rest
+        };
+      });
+    }
+  };
 };
 
-const isPageAndSelector = (obj: ElementGetter | PageAndSelector): obj is PageAndSelector => {
-	return !!(obj as any).page;
-}
+const isBaseContainer = (
+  obj: ElementGetter | BaseElementContainer
+): obj is BaseElementContainer => {
+  return !!(obj as any).page;
+};
 
-export const pupUniDriver = (el: (ElementGetter | PageAndSelector)): UniDriver<ElementContainer> => {
+export const pupUniDriver = (
+  el: ElementGetter | BaseElementContainer
+): UniDriver<ElementContainer> => {
+  const elem = async () => {
+    if (isBaseContainer(el)) {
+      const {page, selector} = el;
+      const element = await page.$(selector);
+      if (!element) {
+        throw new Error(`Cannot find element`);
+      }
+      return {
+        page,
+        element,
+        selector
+      };
+    } else {
+      const {element, ...rest} = await el();
+      if (!element) {
+        throw new Error(`Cannot find element`);
+      }
+      return {
+        ...rest,
+        element
+      };
+    }
+  };
 
-	const elem = async() => {
-		if (isPageAndSelector(el)) {
-			const {page, selector} = el;
-			const element = await page.$(selector);
-			if (!element) {
-				throw new Error(`Cannot find element`);
-			}
-			return {
-				page,
-				element,
-				selector
-			};
-		} else {
-			const {element, ...rest} = await el();
-			if (!element) {
-				throw new Error(`Cannot find element`);
-			}
-			return {
-				...rest,
-				element
-			};
-		}
-	}
+  const exists = async () => {
+    try {
+      await elem();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
 
-	const exists = async () => {
-		try {
-			await elem();
-			return true;
-		} catch (e) {
-			return false;
-		}
-	};
-
-	return {
-		$: (newLoc: Locator) => {
-			return pupUniDriver(async () => {
-				const {element, ...rest} = await elem();
-				return {
-					...rest,
-					element: await element.$(newLoc),
-					selector: newLoc
-				}
-			});
-		},
-		$$: (newLoc: Locator) => pupUniDriverList(async () => {
-			const {element, selector, ...rest} = await elem();
-			return {
-				...rest,
-				elements: await element.$$(newLoc),
-				selector: newLoc
-			}
-		}),
-		text: async () => {
-			const {element} = await elem();
-			const textHandle = await element.getProperty('textContent');
-			const text = await textHandle.jsonValue();
-			return text || '';
-		},
-		click: async () => {
-			return (await elem()).element.click();
-		},
-		hover: async () => {
-			return (await elem()).element.hover();
-		},
-		hasClass: async (className: string) => {
-			const {element} = await elem();
-			const cm: any = await element.getProperty('classList');
-			return cm.indexOf(className) !== -1;
-		},
-		enterValue: async (value: string) => {
-			const {element} = await elem();
-			await element.focus();
-			await element.type(value);
-		},
-		exists,
-		isDisplayed: async () => {
-			const {element} = await elem();
-			return element.isIntersectingViewport();
-		},
-		value: async () => {
-			const {element} = await elem();
-			
-			const valueHandle = await element.getProperty('value');
-			const value = await valueHandle.jsonValue();
-			return value || '';
-		},
-    attr: async name => {
-			const {page, selector} = await elem();
-			return page.$eval(selector, (elem, name) => {
-				return elem.getAttribute(name);
-			}, name);
+  return {
+    $: (newLoc: Locator) => {
+      return pupUniDriver(async () => {
+        const {element, ...rest} = await elem();
+        return {
+          ...rest,
+          element: await element.$(newLoc),
+          selector: newLoc
+        };
+      });
     },
-		wait: async () => {
-			return waitFor(exists);
-		},
-		type: 'puppeteer',
-		scrollIntoView: async () => {
-			const {element} = await elem();
-			await element.hover();
+    $$: (newLoc: Locator) =>
+      pupUniDriverList(async () => {
+        const {element, selector, ...rest} = await elem();
+        return {
+          ...rest,
+          elements: await element.$$(newLoc),
+          selector: newLoc
+        };
+      }),
+    text: async () => {
+      const {element} = await elem();
+      const textHandle = await element.getProperty('textContent');
+      const text = await textHandle.jsonValue();
+      return text || '';
+    },
+    click: async () => {
+      return (await elem()).element.click();
+    },
+    hover: async () => {
+      return (await elem()).element.hover();
+    },
+    hasClass: async (className: string) => {
+      const {element} = await elem();
+      const cm: any = await element.getProperty('classList');
+      return cm.indexOf(className) !== -1;
+    },
+    enterValue: async (value: string) => {
+      const {element} = await elem();
+      await element.focus();
+      await element.type(value);
+    },
+    exists,
+    isDisplayed: async () => {
+      const {element} = await elem();
+      return element.isIntersectingViewport();
+    },
+    value: async () => {
+      const {element} = await elem();
 
-			return {};
-		},
-		getNative: elem
-	};
+      const valueHandle = await element.getProperty('value');
+      const value = await valueHandle.jsonValue();
+      return value || '';
+    },
+    attr: async name => {
+      const {page, selector} = await elem();
+      return page.$eval(
+        selector,
+        (elem, name) => {
+          return elem.getAttribute(name);
+        },
+        name
+      );
+    },
+    wait: async () => {
+      return waitFor(exists);
+    },
+    type: 'puppeteer',
+    scrollIntoView: async () => {
+      const {element} = await elem();
+      await element.hover();
+
+      return {};
+    },
+    getNative: elem
+  };
 };
--- src/lib/puppeteer/spec.ts
+++ src/lib/puppeteer/spec.ts
@@ -1,10 +1,10 @@
-import { runTestSuite }        from '../../test-suite/spec';
+import { runTestSuite } from '../../test-suite/spec';
 import { startServer, getUrl } from '../../test-suite/server';
-import * as puppeteer          from 'puppeteer';
-import { pupUniDriver }        from './index';
-import { TodoAppSetupFn }      from '../../test-suite';
-import { Server }              from 'http';
-import {Browser, Page}         from 'puppeteer';
+import * as puppeteer from 'puppeteer';
+import { pupUniDriver } from './index';
+import { TodoAppSetupFn } from '../../test-suite';
+import { Server } from 'http';
+import { Browser, Page } from 'puppeteer';
 
 const port = 8082;
 
@@ -13,44 +13,33 @@ let browser: Browser;
 let page: Page;
 
 const before = async () => {
-	const args = process.env.CI ? ['--no-sandbox'] : [];
-	const headless = !!process.env.CI;
-	server = await startServer(port);
-	browser = await puppeteer.launch({headless,args});
-	page = await browser.newPage();
+  const args = process.env.CI ? ['--no-sandbox'] : [];
+  const headless = !!process.env.CI;
+  server = await startServer(port);
+  browser = await puppeteer.launch({headless, args});
+  page = await browser.newPage();
 };
 
 const after = async () => {
-	server.close();
-	await page.close();
-	await browser.close();
+  server.close();
+  await page.close();
+  await browser.close();
 };
 
-const setup: TodoAppSetupFn = async (data) => {
+const setup: TodoAppSetupFn = async data => {
+  await page.goto(`http://localhost:${port}${getUrl(data)}`);
+  const driver = pupUniDriver({
+    page,
+    selector: 'body'
+  });
 
-	await page.goto(`http://localhost:${port}${getUrl(data)}`);
-	const driver = pupUniDriver(async() => {
-		const selector = 'body'
-		return{
-			element: await page.$(selector),
-			page,
-			selector
-		}
-	});
+  const tearDown = async () => {};
 
-	const tearDown = async () => { }
-
-	return {driver, tearDown};
-}
-
+  return {driver, tearDown};
+};
 
 describe('puppeteer', () => {
-	runTestSuite({setup, before, after});
+  runTestSuite({setup, before, after});
 });
 
-
-describe('puppeteer specific tests', () => {
-
-
-
-});
+describe('puppeteer specific tests', () => {});
