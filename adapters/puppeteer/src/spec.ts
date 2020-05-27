828935d7e722cbe544011568dcffb33163051661
mykas <mykolass@wix.com>
2020-05-27 15:46:32 +0300

    adapter puppeteer - should scope $

--- adapters/puppeteer/src/spec.ts
+++ adapters/puppeteer/src/spec.ts
@@ -1,8 +1,15 @@
 import * as puppeteer from 'puppeteer';
-import {Browser, Page} from 'puppeteer';
-import {getTestAppUrl, startTestAppServer, SetupFn, runTestSuite} from '@unidriver/test-suite';
-import {pupUniDriver} from './';
-import {Server} from 'http';
+import { assert } from 'chai';
+import { Browser, Page } from 'puppeteer';
+import {
+  getTestAppUrl,
+  startTestAppServer,
+  SetupFn,
+  runTestSuite,
+} from '@unidriver/test-suite';
+import { itemCreator } from '@unidriver/test-suite/dist/utils';
+import { pupUniDriver } from './';
+import { Server } from 'http';
 
 const port = require('find-free-port-sync')();
 
@@ -14,7 +21,7 @@ const before = async () => {
   const args = process.env.CI ? ['--no-sandbox'] : [];
   const headless = !!process.env.CI;
   server = await startTestAppServer(port);
-  browser = await puppeteer.launch({headless, args});
+  browser = await puppeteer.launch({ headless, args });
   page = await browser.newPage();
 };
 
@@ -25,21 +32,33 @@ const after = async () => {
 };
 
 const setup: SetupFn = async (params) => {
-    await page.goto(`http://localhost:${port}${getTestAppUrl(params)}`);
-    const driver = pupUniDriver({
-        page,
-        selector: 'body'
-    });
+  await page.goto(`http://localhost:${port}${getTestAppUrl(params)}`);
+  const driver = pupUniDriver({
+    page,
+    selector: 'body',
+  });
 
-	const tearDown = async () => {};
+  const tearDown = async () => {};
 
-    return {driver, tearDown};
+  return { driver, tearDown };
 };
 
 describe('puppeteer', () => {
-	runTestSuite({setup, before, after});
+  runTestSuite({ setup, before, after });
 });
 
 describe('puppeteer specific tests', () => {
-  
+  beforeEach(() => before());
+
+  afterEach(() => after());
+
+  describe('$', () => {
+    it('should return new driver instance but with scoped value', async () => {
+      const { driver } = await setup({
+        items: [itemCreator({ label: 'Bob' })],
+      });
+
+      assert.equal(await driver.$('header').$('header').exists(), false);
+    });
+  });
 });
