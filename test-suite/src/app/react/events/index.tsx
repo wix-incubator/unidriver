23db79f22bcf44bb9de5f8ae569c95f451422d03
Gabriel Grinberg <gabrielg@wix.com>
2021-02-04 20:40:05 +0200

    test-suite: normalizes Meta key press

--- test-suite/src/app/react/events/index.tsx
+++ test-suite/src/app/react/events/index.tsx
@@ -2,63 +2,64 @@ import * as React from 'react';
 import { EventsAppProps } from '../../../types';
 
 export type EventsAppState = {
-	keyboardEvents: React.KeyboardEvent[];
-	mouseEvents: React.MouseEvent[];
+    keyboardEvents: React.KeyboardEvent[];
+    mouseEvents: React.MouseEvent[];
 }
 
 const keyboardEventComp = (event: React.KeyboardEvent, idx: number) => {
-	 // `whiteSpace: pre` is needed for tests that run in browsers + Space key
+    const normalizedKey = event.key === 'OS' ? 'Meta' : 'OS'; // on FF Meta key press ends up named "OS", so we normalize it (https://stackoverflow.com/questions/39292111/can-firefox-detect-metakey)
+    // `whiteSpace: pre` is needed for tests that run in browsers + Space key
     return (
         <div key={idx} className='keyboard-event-data'>
-            <span className='event-key' style={{whiteSpace: 'pre'}}>{event.key}</span>
+            <span className='event-key' style={{whiteSpace: 'pre'}}>{normalizedKey}</span>
             <span className='event-keycode'>{event.keyCode}</span>
         </div>
     );
 };
 
 const mouseEventComp = (event: React.MouseEvent, idx: number) => {
-	return (
-		<div key={idx} className='mouse-event-data'>
-			<span className='event-type'>{event.type}</span>
-		</div>
-	)
+    return (
+        <div key={idx} className='mouse-event-data'>
+            <span className='event-type'>{event.type}</span>
+        </div>
+    )
 }
 
 export class EventsApp extends React.Component<EventsAppProps, EventsAppState> {
 
-	state = {
-		keyboardEvents: [],
-		mouseEvents: []
-	}
+    state = {
+        keyboardEvents: [],
+        mouseEvents: []
+    }
 
-	onKeyboardEvent = (e: React.KeyboardEvent) => {
-		const {keyboardEvents} = this.state;
-		const eventObj = {...e, key: e.key, keyCode: e.keyCode};
-		this.setState({keyboardEvents: [...keyboardEvents, eventObj]});
-	}
+    onKeyboardEvent = (e: React.KeyboardEvent) => {
+        const {keyboardEvents} = this.state;
+        const eventObj = {...e, key: e.key, keyCode: e.keyCode};
+        this.setState({keyboardEvents: [...keyboardEvents, eventObj]});
+    }
 
-	onMouseEvent = (e: React.MouseEvent) => {
-		const {mouseEvents} = this.state;
-		const eventObj = {...e, type: e.type, clientX: e.clientX, clientY: e.clientY};
-		this.setState({mouseEvents: [...mouseEvents, eventObj]});
-	}
+    onMouseEvent = (e: React.MouseEvent) => {
+        const {mouseEvents} = this.state;
+        const eventObj = {...e, type: e.type, clientX: e.clientX, clientY: e.clientY};
+        this.setState({mouseEvents: [...mouseEvents, eventObj]});
+    }
 
-	render () {
-		const { state } = this;
+    render () {
+        const { state } = this;
 
-		return (
-			<div className='events-container'>
-				<div className='mouse-events'>
-					<button style={{height: 100, width: 100}} onMouseDown={this.onMouseEvent} onMouseUp={this.onMouseEvent} onMouseMove={this.onMouseEvent}>
-						Mouse Events
-					</button>
-					<div>{state.mouseEvents.map(mouseEventComp)}</div>
-				</div>
-				<div className='keyboard-events'>
-					<input onKeyDown={this.onKeyboardEvent} value='' />
-					<div>{state.keyboardEvents.map(keyboardEventComp)}</div>
-				</div>
-			</div>
-		);
-	}
+        return (
+            <div className='events-container'>
+                <div className='mouse-events'>
+                    <button style={{height: 100, width: 100}} onMouseDown={this.onMouseEvent} onMouseUp={this.onMouseEvent} onMouseMove={this.onMouseEvent}>
+                        Mouse Events
+                    </button>
+                    <div>{state.mouseEvents.map(mouseEventComp)}</div>
+                </div>
+                <div className='keyboard-events'>
+                    <input onKeyDown={this.onKeyboardEvent} value='' />
+                    <div>{state.keyboardEvents.map(keyboardEventComp)}</div>
+                </div>
+            </div>
+        );
+    }
 }
