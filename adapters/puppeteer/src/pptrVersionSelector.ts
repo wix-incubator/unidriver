052538cbd85b00cf0cb8d6aa54a770486e71241c
nitayneeman <nitayneeman@users.noreply.github.com>
2022-08-17 11:18:40 +0300

    fix: type mismatch issue

--- adapters/puppeteer/src/pptrVersionSelector.ts
+++ adapters/puppeteer/src/pptrVersionSelector.ts
@@ -1,8 +1,8 @@
-import { ElementHandle as pptrElementHandle, Page as pptrPage, Frame as pptrFrame } from 'puppeteer';
+import { Page as pptrPage, Frame as pptrFrame } from 'puppeteer';
 import { ElementHandle as pptrCoreElementHandle, Page as pptrCorePage, Frame as pptrCoreFrame } from 'puppeteer-core';
 
 export { pptrPage, pptrCorePage};
 
-export type ElementHandle = pptrCoreElementHandle | pptrElementHandle;
+export type ElementHandle = pptrCoreElementHandle;
 export type Page = pptrCorePage | pptrPage;
 export type Frame = pptrCoreFrame | pptrFrame;
